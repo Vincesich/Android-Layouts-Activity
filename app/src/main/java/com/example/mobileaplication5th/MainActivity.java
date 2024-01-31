c32f7ee26bf142efbb60dace54a1ad1ac4b4ca8f
Vincesich <82143726+Vincesich@users.noreply.github.com>
2024-01-31 20:30:43 +0300

    initial commit

---
+++ app/src/main/java/com/example/mobileaplication5th/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mobileaplication5th;
+
+import static com.example.mobileaplication5th.R.id.text1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView;
+    String UnitName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1=findViewById(R.id.text1)
+        UnitName=getString(R.string)
+    }
+
+    private String getString() {
+    }
+}
---
+++ app/src/main/java/com/example/mobileaplication5th/aplication.java
@@ -0,0 +1,14 @@
+package com.example.mobileaplication5th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplication);
+    }
+}
